bde2bfd5f3d68d913652d70cfb6e7958a578b187
unknown <guilhermelbasei@gmail.com>
2022-10-10 16:52:57 -0300

    Imagens

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,59 +1,107 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 import './styles.css'
 
 export const Item = () => {
+  const [itens, setItens] = useState([
+    {
+      name: "Maminha",
+      value: 45.90,
+      link: 'https://io.convertiez.com.br/m/superpaguemenos/shop/products/images/14380/medium/maminha-bovina-leve-mais-resfriada-kg_10762.jpg',
+    },
+    {
+      name: "Picanha",
+      value: 99.90,
+      link: 'https://branguscarnes.com.br/wp-content/uploads/2021/05/picanha-brangus.png',
+    },
+    {
+      name: "Patinho",
+      value: 33.90,
+      link: 'https://www.madrid.com.br/imagens/produtos/big/30062.jpg',
+    },
+    {
+      name: "Cupim",
+      value: 48.90,
+      link: 'https://io.convertiez.com.br/m/superpaguemenos/shop/products/images/14368/medium/cupim-bovino-resfriado-kg_10757.jpg',
+    },
+    {
+      name: "Costela",
+      value: 25.90,
+      link: 'https://io.convertiez.com.br/m/superpaguemenos/shop/products/images/14343/medium/costela-bovina-especial-resfriada-kg_92881.png',
+    },
+    {
+      name: "Carvão",
+      value: 5.00,
+      link: 'https://atacadaotopburguer.com.br/wp-content/uploads/2020/08/carvao25kg.png',
+    },
+    {
+      name: "Espetos",
+      value: 27.90,
+      link: 'https://png.pngtree.com/png-vector/20210205/ourlarge/pngtree-bbq-lamb-skewers-png-image_2873841.jpg',
+    }
+  ]);
+
+
+
+
   return (
-    <div class="container">
-      <div class="row">
-   
-        <div class="col-sm">
-        <Link to={"/monoproduto"}>
-          <img className='imgProd' src='https://avatars.githubusercontent.com/u/90064992?v=4' alt="shoe" />
-          <br></br>
-          <span>
-           Carne: Picanha
-          </span><br></br>                  
-          <span>
-            Valor: R$150,00
-          </span><br></br>     
-     
-          </Link>
-          <button>Adicionar ao carrinho</button>
-        </div>
+    <div className='container'>
+      {itens.map(item => {
+        return (
 
-        <div class="col-sm">
-        <Link to={"/monoproduto"}>
-          <img className='imgProd' src='https://avatars.githubusercontent.com/u/90064992?v=4' alt="shoe" />
-          <br></br>
-          <span>
-           Carne: Picanha
-          </span><br></br>                  
-          <span>
-            Valor: R$150,00
-          </span><br></br>     
-     
-          </Link>
-          <button>Adicionar ao carrinho</button>
-        </div>
+          <div class="container">
+            <div class="row">
 
-        <div class="col-sm">
-        <Link to={"/monoproduto"}>
-          <img className='imgProd' src='https://avatars.githubusercontent.com/u/90064992?v=4' alt="shoe" />
-          <br></br>
-          <span>
-           Carne: Picanha
-          </span><br></br>                  
-          <span>
-            Valor: R$150,00
-          </span><br></br>     
-     
-          </Link>
-          <button>Adicionar ao carrinho</button>
-        </div>
-      </div>
+              <div class="col-sm">
+                <Link to={"/monoproduto"}>
+                  <img className='imgProd' src={item.link} alt="" />
+                  <br></br>
+                  <span>
+                    {item.name}
+                  </span><br></br>
+                  <span>
+                    R${item.value}KG
+                  </span><br></br>
 
+                </Link>
+                <button>Adicionar ao carrinho</button>
+              </div>
+
+              <div class="col-sm">
+                <Link to={"/monoproduto"}>
+                  <img className='imgProd' src={item.link} alt="" />
+                  <br></br>
+                  <span>
+                    {item.name}
+                  </span><br></br>
+                  <span>
+                    R${item.value}KG
+                  </span><br></br>
+
+                </Link>
+                <button>Adicionar ao carrinho</button>
+              </div>
+
+              <div class="col-sm">
+                <Link to={"/monoproduto"}>
+                  <img className='imgProd' src={item.link} alt="" />
+                  <br></br>
+                  <span>
+                    {item.name}
+                  </span><br></br>
+                  <span>
+                    R${item.value}KG
+                  </span><br></br>
+
+                </Link>
+                <button>Adicionar ao carrinho</button>
+              </div>
+            </div>
+          </div>
+        )
+      })
+      }
 
     </div>
   )
 }
-
--- src/components/Itens/index.jsx
+++ src/components/Itens/index.jsx
@@ -5,32 +5,39 @@ import './styles.css';
 export const Itens = () => {
     const [itens, setItens] = useState([
         {
-            name: "Maminhona",
-            value: 150.00,
+            name: "Maminha",
+            value: 45.90,
+            link: 'https://io.convertiez.com.br/m/superpaguemenos/shop/products/images/14380/medium/maminha-bovina-leve-mais-resfriada-kg_10762.jpg',
         },
         {
-            name: "Maminhona",
-            value: 150.00,
+            name: "Picanha",
+            value: 99.90,
+            link: 'https://branguscarnes.com.br/wp-content/uploads/2021/05/picanha-brangus.png',
         },
         {
-            name: "Maminhona",
-            value: 150.00,
+            name: "Patinho",
+            value: 33.90,
+            link: 'https://www.madrid.com.br/imagens/produtos/big/30062.jpg',
         },
         {
-            name: "Maminhona",
-            value: 150.00,
+            name: "Cupim",
+            value: 48.90,
+            link: 'https://io.convertiez.com.br/m/superpaguemenos/shop/products/images/14368/medium/cupim-bovino-resfriado-kg_10757.jpg',
         },
         {
-            name: "Maminhona",
-            value: 150.00,
+            name: "Costela",
+            value: 25.90,
+            link: 'https://io.convertiez.com.br/m/superpaguemenos/shop/products/images/14343/medium/costela-bovina-especial-resfriada-kg_92881.png',
         },
         {
-            name: "Maminhona",
-            value: 150.00,
+            name: "Carvão",
+            value: 5.00,
+            link: 'https://atacadaotopburguer.com.br/wp-content/uploads/2020/08/carvao25kg.png',
         },
         {
-            name: "Maminhona",
-            value: 150.00,
+            name: "Espetos",
+            value: 27.90,
+            link: 'https://png.pngtree.com/png-vector/20210205/ourlarge/pngtree-bbq-lamb-skewers-png-image_2873841.jpg',
         }
     ]);
     var settings = {
@@ -51,7 +58,7 @@ export const Itens = () => {
                         <div className="container-item">
                             <div class="item">
                                 <div className='image'>
-                                    <img src='https://avatars.githubusercontent.com/u/90064992?v=4' alt="shoe" />
+                                    <img src={item.link} alt="shoe" />
                                 </div>
                                 <div className='info'>
                                     <span className='name'>{item.name}</span>
